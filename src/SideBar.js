b59df5b65ca9158ed19026c984c7145e1b545706
UnoMartino <uno.martino@icloud.com>
2022-02-15 11:11:05 +0100

    initial commit

---
+++ src/SideBar.js
@@ -0,0 +1,21 @@
+import { BsPlus, BsFillLightningFill } from 'react-icons/bs';
+import { FaFire, FaPoo } from 'react-icons/fa';
+
+const SideBar = () => {
+  return (
+    <div className="...">
+      <SideBarIcon icon={<FaFire size="28" />} />
+      <SideBarIcon icon={<BsPlus size="32" />} />
+      <SideBarIcon icon={<BsFillLightningFill size="20" />} />
+      <SideBarIcon icon={<FaPoo size="20" />} />
+    </div>
+  );
+};
+
+const SideBarIcon = ({ icon }) => (
+  <div className="sidebar-icon group">
+    {icon}
+
+    <span class="sidebar-tooltip group-hover:scale-100">{text}</span>
+  </div>
+);
