99d8269cead6b8307f5304cffe0e4fee5e6109d7
UnoMartino <69631058+UnoMartino@users.noreply.github.com>
2022-09-18 13:39:01 +0200

    fixes

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 CONFIGURED="CONFIGURED"
 if [ ! -e /data/$CONFIGURED ]; then
     echo "-- Container not configured --"
-    sed -i 's/webbrowser.open(auth.url)/print(auth.url)/g' /usr/lib/python3.10/site-packages/tekore/_auth/util.py
+    sed -i 's/webbrowser.open(auth.url)/print(auth.url)/g' /usr/lib/python*/site-packages/tekore/_auth/util.py
     python3 /data/backend.py configure $SPOTIFY_CLIENT_ID $SPOTIFY_CLIENT_SECRET $SPOTIFY_USERNAME $SPOTIFY_REVIEW_PLAYLIST_ID $DB_HOST $DB_USER $DB_PASSWORD $DB_DATABASE $SPOTIFY_REJECTED_PLAYLIST_ID $SPOTIFY_BLACKLIST_SONGS_PLAYLIST_ID $SPOTIFY_BLACKLIST_ARTISTS_PLAYLIST_ID $SPOTIFY_WHITELIST_SONGS_PLAYLIST_ID $SPOTIFY_WHITELIST_ARTISTS_PLAYLIST_ID $SPOTIFY_QUEUE_PLAYLIST_ID
     touch /data/$CONFIGURED
 else
