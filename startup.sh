ac627fbfb56d220f7272a09ad57046dd70a467bc
UnoMartino <69631058+UnoMartino@users.noreply.github.com>
2022-09-18 13:33:06 +0200

    changed backend to alpine

--- startup.sh
+++ startup.sh
@@ -1,10 +1,10 @@
 CONFIGURED="CONFIGURED"
 if [ ! -e /data/$CONFIGURED ]; then
     echo "-- Container not configured --"
-    sed -i 's/webbrowser.open(auth.url)/print(auth.url)/g' /usr/local/lib/python3.10/dist-packages/tekore/_auth/util.py
+    sed -i 's/webbrowser.open(auth.url)/print(auth.url)/g' /usr/lib/python3.10/site-packages/tekore/_auth/util.py
     python3 /data/backend.py configure $SPOTIFY_CLIENT_ID $SPOTIFY_CLIENT_SECRET $SPOTIFY_USERNAME $SPOTIFY_REVIEW_PLAYLIST_ID $DB_HOST $DB_USER $DB_PASSWORD $DB_DATABASE $SPOTIFY_REJECTED_PLAYLIST_ID $SPOTIFY_BLACKLIST_SONGS_PLAYLIST_ID $SPOTIFY_BLACKLIST_ARTISTS_PLAYLIST_ID $SPOTIFY_WHITELIST_SONGS_PLAYLIST_ID $SPOTIFY_WHITELIST_ARTISTS_PLAYLIST_ID $SPOTIFY_QUEUE_PLAYLIST_ID
     touch /data/$CONFIGURED
 else
     echo "-- Container configured --"
-    gunicorn -w 9 -b 0.0.0.0 backend:app
+    python3 /data/backend.py start
 fi
